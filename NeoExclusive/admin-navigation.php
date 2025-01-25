f0256c2603abcf2258f49e306c4f2e47e7cb9fb5
kinavee <149876107+kinavee@users.noreply.github.com>
2025-01-25 16:50:45 +0800

    Add files via upload

---
+++ NeoExclusive/admin-navigation.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Neo Cafe Admin Page</title>
+  <style>
+    body {
+      margin: 0;
+      font-family: sans-serif;
+    }
+    .header {
+      background-color: #a4856b;
+      color: #228b22;
+      padding: 10px;
+      text-align: center;
+    }
+    .sidebar {
+      background-color: #d3d3d3;
+      padding: 20px;
+      height: 100%;
+      float: left;
+      width: 20%;
+    }
+    .content {
+      padding: 20px;
+      height: 100%;
+      float: left;
+      width: 80%;
+    }
+  </style>
+</head>
+<body>
+  <div class="header">
+    <h1>Neo Cafe Admin Page</h1>
+    <p style="float:right">Profile</p>
+  </div>
+
+  <div class="sidebar">
+    <a href="#">Home</a>
+    <a href="#">Orders</a>
+    <a href="#">Products</a>
+    <a href="#">Transactions</a>
+    <a href="#">Blog</a>
+    <a href="#">Archive</a>
+  </div>
+
+  <div class="content">
+    
+  </div>
+</body>
+</html>
